31d8bc3e04f0e0a8affdfa991fec4009305b57b6
benson <mwangibenson923@gmail.com>
2024-07-19 20:08:18 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+      name: userName,
+      email: userEmail,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(function (object) {
+        document.body.innerHTML += object.id;
+      })
+      .catch(function (error) {
+        document.body.innerHTML += error.message;
+      });
+  }
+  // Add your code here
